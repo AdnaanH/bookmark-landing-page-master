2bc91b00d98f5ffc194c724e1b1ba223b5ae1d44
AdnaanH <60857004+AdnaanH@users.noreply.github.com>
2023-10-29 15:54:05 +0300

    Accordion Section, Features Section & Mobile menu

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+var isMenuOpen = false;  // Initialize a flag to track the menu state
+
 document.getElementById("ham-menu").addEventListener("click", function() {
     var menuArea = document.getElementById("menu-area");
     var hamMenu = document.getElementById("ham-menu");
     var socialsTop = document.getElementById("socials-top");
 
-    if (menuArea.style.display === "flex") {
+    if (isMenuOpen) {
         menuArea.style.display = "none";
         hamMenu.classList.remove("bi-x");
         hamMenu.classList.add("bi-list");
@@ -13,11 +15,47 @@ document.getElementById("ham-menu").addEventListener("click", function() {
         hamMenu.classList.remove("bi-list");
         hamMenu.classList.add("bi-x");
         socialsTop.style.display = "flex";
-
-        hamMenu.addEventListener("click", function() {
-            menuArea.style.display = "none";
-            hamMenu.classList.remove("bi-x");
-            hamMenu.classList.add("bi-list");
-        });
     }
+
+    isMenuOpen = !isMenuOpen;  // Toggle the menu state
+});
+
+
+
+const featSelElements = document.querySelectorAll('.feat-sel');
+const featureElements = document.querySelectorAll('.feature');
+
+featSelElements.forEach((selector, index) => {
+  selector.addEventListener('click', () => {
+    featSelElements.forEach((feature) => {
+      feature.classList.remove('feat-active');
+    });
+
+    selector.classList.add('feat-active');
+
+    featureElements.forEach((feature) => {
+      feature.style.display = 'none';
+    });
+
+    featureElements[index].style.display = 'flex';
+  });
+});
+
+const faqElements = document.querySelectorAll('.faq');
+
+faqElements.forEach((faq) => {
+  const chevronIcon = faq.querySelector('.faq-q i');
+  const answer = faq.querySelector('.faq-a');
+
+  faq.addEventListener('click', () => {
+    if (answer.style.display === 'grid') {
+      answer.style.display = 'none';
+      chevronIcon.classList.remove('bi-chevron-up');
+      chevronIcon.classList.add('bi-chevron-down');
+    } else {
+      answer.style.display = 'grid';
+      chevronIcon.classList.remove('bi-chevron-down');
+      chevronIcon.classList.add('bi-chevron-up');
+    }
+  });
 });
